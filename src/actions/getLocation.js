0b806e37c3d50dfd198eb95352cc45329be98f59
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-09-02 10:52:11 +0100

    changed the location from country to city

--- src/actions/getLocation.js
+++ src/actions/getLocation.js
@@ -5,7 +5,7 @@ const getLocation = ({ setLocation }) => {
     .get(
       "https://ipgeolocation.abstractapi.com/v1/?api_key=d90cd3aecf1b462ca5b352334f6616a5"
     )
-    .then((res) => setLocation(res.data.country))
+    .then((res) => setLocation(res.data.city))
     .catch(() => console.error("Something went wrong!"));
 };
 
